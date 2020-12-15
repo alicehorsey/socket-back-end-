611e3b64374949d940ad1462f298a62b9ea29dea
Alice Horsey <alicehorsey@gmail.com>
2020-12-15 15:12:16 +0000

    back end socket app

---
+++ index.js
@@ -0,0 +1,33 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http, {
+    cors: {
+        origin: true,
+        methods: ["GET", "POST"],
+        // "Access-Control-Allow-Origin": "*",
+    }
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+    console.log("testing function")
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
+
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+
+//         io.emit('chat message', msg);
+//     });
+// });
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
